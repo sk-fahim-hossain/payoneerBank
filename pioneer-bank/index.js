f2a15b2ce7d68673eed909a7c397ea74757cc68e
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2021-12-01 00:42:47 +0600

    ready to go payoneer bank site

---
+++ pioneer-bank/index.js
@@ -0,0 +1,50 @@
+//login btn event hundler
+const loginBtn = document.getElementById("login")
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+//deposit event hundler
+const depositBtn = document.getElementById("depositBtn");
+depositBtn.addEventListener("click", function(){
+    
+    const depositNumber = getInputNumber("depositAmount")
+
+    updateSpanText("currentDeposit",depositNumber);
+    updateSpanText("currentBalance",depositNumber);
+
+    document.getElementById("depositAmount").value = "";
+
+})
+
+//withdraw event hundler
+    const withdrawBtn = document.getElementById("withdrawBtn");
+    withdrawBtn.addEventListener("click",function(){
+        const withdrawNumber = getInputNumber("withdrawAmount");
+      
+
+        
+        updateSpanText("currentWithdraw",withdrawNumber);
+        updateSpanText("currentBalance",-1 * withdrawNumber);
+        document.getElementById("withdrawAmount").value = "";
+    })
+
+function getInputNumber(id){
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}    
+
+function updateSpanText(id,depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
+
+
+
+
